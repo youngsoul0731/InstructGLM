87fd6a4a3e1defc91fb3b6f5cee7506f47addb96
Yuanshuo Zhang <youngsoul0731@gmail.com>
2024-04-23 18:12:02 +0800

    Update pretrain.py

--- llama_arxiv_src/pretrain.py
+++ llama_arxiv_src/pretrain.py
@@ -20,7 +20,7 @@ from param import parse_args
 from pretrain_data import get_loader,load_pickle 
 from utils import LossMeter
 from dist_utils import reduce_dict, new_reduce_dict
-
+from apex import amp
 from peft import (    # LoRA Setting
     LoraConfig,
     get_peft_model,
